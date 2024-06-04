e73f27252e199cd6afe561990759ec87f7c96c70
Kalpesh446 <Kalpeshchavda446@gmail.com>
2024-06-04 18:48:01 +0530

    routing add

--- E-commerce website/src/main.jsx
+++ E-commerce website/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store.js";
@@ -12,9 +11,7 @@ import "slick-carousel/slick/slick-theme.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </PersistGate>
   </Provider>
 );
